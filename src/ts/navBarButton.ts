a7c929dfa4d9c8955e3b7f27cc814481c2915ef7
Emilie <emilie.marion.simplon@gmail.com>
2025-08-27 14:09:25 +0200

    added burger menu + functionality + added rollup and co.

---
+++ src/ts/navBarButton.ts
@@ -0,0 +1,31 @@
+const navBar: HTMLElement | null = document.getElementById("headerNavBar");
+const button: HTMLElement | null = document.getElementById("navBarButton");
+
+if(navBar && button){
+    button.addEventListener("click", navBarToggled);
+}
+
+window.addEventListener("resize", screenIsResized);
+
+function screenIsResized(){
+    if(navBar){
+        if (screen.width > 768){
+            navBar.style.display = "flex";
+        }
+        else {
+            navBar.style.display = "none";
+        }
+    }
+}
+
+function navBarToggled(){
+    if(navBar){
+        const display: string = navBar.style.display;
+        if(display === "none"){
+             navBar.style.display = "flex";
+        }
+        else{
+            navBar.style.display = "none";
+        }
+    }
+}
